35303783c2a16776dc5e969b286b2940a720c427
Michele Riva <ciao@micheleriva.it>
2022-08-15 17:54:53 +0200

    first commit

---
+++ utils/types.ts
@@ -0,0 +1,13 @@
+export type GithubStars = {
+  user: string;
+  repo: string;
+  size: string;
+};
+
+export type YoutubeProps = {
+  embedId: string;
+};
+
+export type DemoButtonProps = {
+  link: string;
+};
